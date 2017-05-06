f3b4750c6a070a4b4163319fc59987749b28f4bb
Justin king <jck2011@hotmail.co.uk>
2017-05-06 20:35:06 +0100

    Main Menu

---
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+    public InputField iField;
+
+    void Start() {
+        int rSeed = Random.Range(11111, 99999);
+        iField.text = System.Convert.ToString(rSeed);
+    }
+    public void LoadByIndex(int sceneIndex) {
+        SceneManager.LoadScene(sceneIndex);
+        //int rSeed = Random.Range(0, 99999);
+        //int rSeed = 2000;
+        int rSeed = System.Convert.ToInt32(iField.text);
+        PlayerPrefs.SetInt("rSeed",rSeed);
+    }
+}
